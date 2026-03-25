5af1d08069429f48d4b54c0bd6055f0358227ed2
alskra <krasnoyurchenko.a.s@gmail.com>
2026-03-25 15:42:33 +0300

    update

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,11 +1,7 @@
 const advantagesSlider = new Swiper('.advantages__slider', {
 	slidesPerView: 'auto',
 	spaceBetween: 0,
-	mousewheel: true,
-	// centeredSlides: true,
-	// breakpoints: {
-	// 	1025: {
-	// 		centeredSlides: false,
-	// 	},
-	// }
+	mousewheel: {
+		releaseOnEdges: true,
+	},
 });
